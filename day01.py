7357c162aff2106a00a3be2a66ea5f38a701817b
Ethan Shafer <ethan.shafer@westpoint.edu>
2024-12-05 17:20:05 -0500

    Day 1 Part 2

--- day01.py
+++ day01.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 import re
 from utilities import *
+from collections import Counter
 
 filename = Path(sys.modules[__name__].__file__).name
 pattern = re.compile(r'\d+')
@@ -26,3 +27,19 @@ def part1():
 
     result = sum([abs(val - col2[index]) for index, val in enumerate(col1)])
     print(result)
+
+def part2():
+    ACTIVE_TEXT = TEXT
+
+    col1, col2 = [], []
+    for row in ACTIVE_TEXT:
+        val1, val2 = [int(x) for x in row.split()]
+        col1.append(val1)
+        col2.append(val2)
+
+    count = Counter(col2)
+    result = sum([val * count[val] for val in col1])
+    print(result)
+    return result
+
+part2()
