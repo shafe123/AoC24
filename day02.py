c8803af68926fdad24f4f827fe5c2ed7ee6984be
Ethan Shafer <ethan.shafer@westpoint.edu>
2024-12-05 17:33:45 -0500

    Day 2 Part 1

---
+++ day02.py
@@ -0,0 +1,34 @@
+import sys
+from pathlib import Path
+import re
+from utilities import *
+
+filename = Path(sys.modules[__name__].__file__).name
+pattern = re.compile(r'\d+')
+match = pattern.search(filename)
+day = match.group()
+
+SAMPLE_TEXT = read_file(Path(f'./data/day{day}_sample.txt'))
+TEXT = read_file(Path(f'./data/day{day}.txt'))
+
+def part1():
+    ACTIVE_TEXT = TEXT
+
+    safe_rows = []
+    for row in ACTIVE_TEXT:
+        vals = [int(x) for x in row.split()]
+
+        # ascending check
+        if all([0 < y - x < 4 for x, y in zip(vals[::], vals[1::])]):
+            safe_rows.append(vals)
+            continue
+
+        # descending check
+        if all([0 < x - y < 4 for x, y in zip(vals[::], vals[1::])]):
+            safe_rows.append(vals)
+            continue
+
+    return len(safe_rows)
+
+print(part1())
+
