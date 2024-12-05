8e3271a291490ff0a5dec3dc296c8616679c955b
Ethan Shafer <ethan.shafer@westpoint.edu>
2024-12-05 17:17:11 -0500

    Day 1 Part 1

--- day01.py
+++ day01.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import re
 from utilities import *
 
-print(__name__)
 filename = Path(sys.modules[__name__].__file__).name
 pattern = re.compile(r'\d+')
 match = pattern.search(filename)
@@ -12,13 +11,18 @@ day = match.group()
 SAMPLE_TEXT = read_file(Path(f'./data/day{day}_sample.txt'))
 TEXT = read_file(Path(f'./data/day{day}.txt'))
 
-@separator('SAMPLE')
-def print_sample():
-    print(SAMPLE_TEXT)
+def part1():
+    ACTIVE_TEXT = TEXT
 
-@separator('ACTUAL')
-def print_actual():
-    print(TEXT)
+    col1, col2 = [], []
+    for row in ACTIVE_TEXT:
+        val1, val2 = [int(x) for x in row.split()]
+        col1.append(val1)
+        col2.append(val2)
 
-print_sample()
-print_actual()
+    col1.sort()
+    col2.sort()
+
+
+    result = sum([abs(val - col2[index]) for index, val in enumerate(col1)])
+    print(result)
